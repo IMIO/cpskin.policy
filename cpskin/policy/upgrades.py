a996419d87b2dda41ce4eb2cb07ef2e8f7a1eb67
Benoît Suttor <benoit.suttor@imio.be>
2015-03-12 14:00:49 +0100

    Add upgrade step which install collective.atomrss.

--- cpskin/policy/upgrades.py
+++ cpskin/policy/upgrades.py
@@ -11,3 +11,14 @@ def delete_multilingualbehavior(context, logger=None):
     setup_tool.runAllImportStepsFromProfile(
         'profile-plone.multilingualbehavior:uninstall', purge_old=False)
     logger.info('plone.multilingualbehavior deleted')
+
+
+def install_collective_atomrss(context, logger=None):
+    if logger is None:
+        # Called as upgrade step: define our own logger.
+        logger = logging.getLogger('cpskin.policy')
+
+    setup_tool = getToolByName(context, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile(
+        'profile-collective.atomrss:default')
+    logger.info('collective.atomrss installed')
